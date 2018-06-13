c014edcf8f62710aeeb6fbed0d535cce84f6d9f6
KameliyaIvanova <iv.kameliya@gmail.com>
2018-06-14 01:10:22 +0300

    Set public folder

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const handlebars = require('express-handlebars')
 
 mongoose.Promise = global.Promise
 
 let env = process.env.NODE_ENV || 'development'
-let port = process.env.PORT || 13337
+let port = process.env.PORT || 1337
 
 let app = express()
+
+app.engine('.hbs', handlebars({
+  extname: '.hbs',
+  layoutsDir: 'views/layouts',
+  defaultLayout: 'main'
+}))
+
+app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => {
   mongoose.connect('mongodb://localhost:27017/ganerictemplate')
     .then(() => {
       console.log('MongoDb Ready!')
 
-      res.send('Hi!')
+      res.render('index')
     })
 })
-
+app.use(express.static('public'))
 app.listen(port)
 console.log(`Server listening on port ${port}...`)
